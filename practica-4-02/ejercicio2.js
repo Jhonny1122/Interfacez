910a37a1e566f3fd4fbf6d0dd7e3a71287aa2025
Jhonny1122 <113983932+Jhonny1122@users.noreply.github.com>
2023-02-20 13:55:45 +0100

    Add files via upload

---
+++ practica-4-02/ejercicio2.js
@@ -0,0 +1,13 @@
+$(window).on('load', calendario);
+
+// La fecha se adquiera con un calendario de jQueryUI.
+function calendario() {
+
+    $( "#nacimiento" ).datepicker();
+
+}
+
+// Cuando nos situemos sobre cada uno de los controles nos muestre una ventana de ayuda.
+function ventanas(){}
+
+
